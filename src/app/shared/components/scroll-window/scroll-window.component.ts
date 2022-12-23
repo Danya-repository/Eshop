a17df406779c1765016765019c23a643012b2b93
d.kashapov <d.kashapov@tinkoff.ru>
2022-12-23 22:38:31 +0300

    create animation for scrollbar

--- src/app/shared/components/scroll-window/scroll-window.component.ts
+++ src/app/shared/components/scroll-window/scroll-window.component.ts
@@ -1,146 +1,52 @@
 import { AfterContentChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ScrollWindowService } from '../../services/scroll-window.service';
 
 @Component({
   selector: 'app-scroll-window',
   templateUrl: './scroll-window.component.html',
-  styleUrls: ['./scroll-window.component.scss']
+  styleUrls: ['./scroll-window.component.scss'],
+  providers: [ScrollWindowService]
 })
 export class ScrollWindowComponent implements AfterContentChecked {
 
   @ViewChild('container', {static: true}) container!: ElementRef;
   @ViewChild('handle', {static: false}) handle!: ElementRef;
   @ViewChild('window', {static: true}) window!: ElementRef;
+  @ViewChild('strip', {static: false}) strip!: ElementRef;
 
-  handlePosition: number = 0;
-  startMouseHandlePosition: number = 0;
-  draggable: boolean = false;
-  canUp: boolean = true;
-  canDown: boolean = true;
-  windowHeight!: number;
-  containerHeight!: number;
-  scrollValue: number = 0;
-  containerPosition: number = 0;
-  transition: boolean = true;
+  
+  constructor(protected scrollWindowService: ScrollWindowService) { }
 
-
-  constructor() { }
   ngAfterContentChecked(): void {
-    this.windowHeight = this.window.nativeElement.offsetHeight;
-    this.containerHeight = this.container.nativeElement.offsetHeight;
+    this.scrollWindowService.initialize(this.window, this.container, this.strip, this.handle)
   }
 
   mousedown(event: MouseEvent) {
+    event.stopPropagation();
     event.preventDefault();
-    if (!this.handle.nativeElement.contains(event.target)) return
-    this.draggable = true;
-    this.startMouseHandlePosition = event.clientY;
-    this.transition = false;
+
+    this.scrollWindowService.mousedown(event)
   }
 
   mousemove(event: MouseEvent) {
     event.stopPropagation();
     event.preventDefault();
-    if (!this.draggable) return
-
-    // драг вверх
-    if (this.startMouseHandlePosition < event.clientY) {
-      this.canUp = true;
-
-
-      if (this.handlePosition > this.window.nativeElement.offsetHeight - 40) {
-        this.canDown = false;
-        this.handlePosition = this.window.nativeElement.offsetHeight - 40;
-        this.scrollValue = 100;
-      }
-      
-      if (this.canDown) {
-        this.handlePosition -= this.startMouseHandlePosition - event.clientY;
-        this.startMouseHandlePosition = event.clientY  
-        this.scrollValue = Math.round(this.handlePosition * 100 / (this.windowHeight - 40));
-
-        this.containerPosition = -(this.containerHeight - this.windowHeight) / 100 * this.scrollValue;
-        
-      }
-    }
-
-    // драг вниз
-    if (this.startMouseHandlePosition > event.clientY) {
-      this.canDown = true;
-
-
-      if (this.handlePosition < 0) {
-        this.canUp = false;
-        this.handlePosition = 0;
-        this.scrollValue = 0;
-      }
-
-
-      if (this.canUp) {
-        this.handlePosition -= this.startMouseHandlePosition - event.clientY;
-        this.startMouseHandlePosition = event.clientY   
-        this.scrollValue = Math.round(this.handlePosition * 100 / (this.windowHeight - 40));
-
-        this.containerPosition = -(this.containerHeight - this.windowHeight) / 100 * this.scrollValue;
-      }  
-    }
-
+    
+    this.scrollWindowService.mousemove(event)
   }
 
-  mouseup(event: MouseEvent) {
-    if (!this.draggable) return;
-    this.draggable = false;
-    this.startMouseHandlePosition = this.handlePosition;
-    this.transition = true;
+  mouseup() {
+    this.scrollWindowService.mouseup()
   }
 
-  mouseleave(event: MouseEvent) {
-    this.mouseup(event)
+  mouseleave() {
+    this.scrollWindowService.mouseleave()
   }
 
   mousescroll(event: WheelEvent) {
     event.stopPropagation();
     event.preventDefault();
 
-    let prevHandlePosition = this.handlePosition;
-    let nextHandlePosition = prevHandlePosition + event.deltaY / 5;
-
-    // скролл вверх
-    if (prevHandlePosition > nextHandlePosition) {
-      this.canDown = true;
-
-
-      if (nextHandlePosition < 0) {
-        this.canUp = false;
-        this.handlePosition = 0;
-        this.scrollValue = 0;
-      }
-
-
-      if (this.canUp) {
-        this.handlePosition = nextHandlePosition;
-        this.scrollValue = Math.round(this.handlePosition * 100 / (this.windowHeight - 40));
-        this.containerPosition = -(this.containerHeight - this.windowHeight) / 100 * this.scrollValue;
-      }      
-    }
-
-    // Скролл вниз
-    if (prevHandlePosition < nextHandlePosition) {
-      this.canUp = true;
-      // this.container.nativeElement
-
-
-      if (nextHandlePosition > this.window.nativeElement.offsetHeight - 40) {
-        this.canDown = false;
-        this.handlePosition = this.window.nativeElement.offsetHeight - 40;
-        this.scrollValue = 0;
-      }
-
-
-      if (this.canDown) {
-        this.handlePosition = nextHandlePosition;
-        this.scrollValue = Math.round(this.handlePosition * 100 / (this.windowHeight - 40));
-        this.containerPosition = -(this.containerHeight - this.windowHeight) / 100 * this.scrollValue;
-      }  
-    }
+    this.scrollWindowService.mousescroll(event);
   }
 }
